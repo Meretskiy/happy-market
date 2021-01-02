c46c2eaaa9592eda7bed407791f1eb31deff1ca2
Meretskiy Aleksandr <a-meretskiy@mail.ru>
2021-01-02 18:41:57 +0300

    first commit

---
+++ src/main/java/com/meretskiy/internet/market/HappyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.meretskiy.internet.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meretskiy/internet/market/HappyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meretskiy.internet.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
